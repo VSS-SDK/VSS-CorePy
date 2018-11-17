40e563044c2ea1097bfea37f9d4639c957f64e9b
John Fercher <johnathanfercher22@gmail.com>
2018-11-17 19:48:13 -0200

    alterado nome do pacote

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='vss_corepy',
     version='0.1',
-    packages=['src',],
+    packages=['vss_corepy',],
     license='GPL3',
     url='https://vss-sdk.github.io/',
     author_email='johnathanfercher22@gmail.com',
