9eebcf3e9e370002bb9c4a674bd068650712339a
John Fercher <johnathanfercher22@gmail.com>
2018-11-17 19:42:04 -0200

    adicionado coisas

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='VSS-CorePy',
+    version='0.1',
+    packages=['vss-corepy',],
+    license='GPL3',
+    url='https://vss-sdk.github.io/',
+    author_email='johnathanfercher22@gmail.com',
+    long_description=open('README.md').read(),
+)
---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='VSS-CorePy',
+    version='0.1',
+    packages=['vss-corepy',],
+    license='GPL3',
+    long_description=open('README.md').read(),
+)
