deae3fa3a2661c727fcead44eb12a806cbbc87bd
John Fercher <johnathanfercher22@gmail.com>
2018-11-17 20:50:09 -0200

    find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='vsscorepy',
     version='0.1.1',
-    packages=['vsscorepy',],
+    packages=find_packages(),
     license='GPL3',
     url='https://vss-sdk.github.io/',
     author_email='johnathanfercher22@gmail.com',
