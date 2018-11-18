91db90462745506ae4a3fc8cc94890f56280b23b
John Fercher <johnathanfercher22@gmail.com>
2018-11-18 11:50:48 -0200

    retornando com pasta src

--- setup.py
+++ setup.py
@@ -1,11 +1,69 @@
-from setuptools import setup, find_packages
+import subprocess
+import os
+import sys
+import shutil
 
-setup(
-    name='vsscorepy',
-    version='0.1.1',
-    packages=find_packages(),
-    license='GPL3',
-    url='https://vss-sdk.github.io/',
-    author_email='johnathanfercher22@gmail.com',
-    keywords=["IEEE", "VSS", "Robot Soccer", "VSS-SDK"],
-)
+from setuptools import setup, find_packages
+from distutils.spawn import find_executable
+
+# Find the Protocol Compiler.
+if 'PROTOC' in os.environ and os.path.exists(os.environ['PROTOC']):
+    protoc = os.environ['PROTOC']
+elif os.path.exists("../src/protoc"):
+    protoc = "../src/protoc"
+elif os.path.exists("../src/protoc.exe"):
+    protoc = "../src/protoc.exe"
+elif os.path.exists("../vsprojects/Debug/protoc.exe"):
+    protoc = "../vsprojects/Debug/protoc.exe"
+elif os.path.exists("../vsprojects/Release/protoc.exe"):
+    protoc = "../vsprojects/Release/protoc.exe"
+else:
+    protoc = find_executable("protoc")
+
+def generate_state():
+    protoc_command = [ protoc, "-I/src/protos", "-I.", "--python_out=.", "protos/state.proto" ]
+
+    if subprocess.call(protoc_command) != 0:
+        sys.exit(-1)
+
+    shutil.move("protos/state_pb2.py", "src/protos/state_pb2.py")
+
+def generate_command():
+    protoc_command = [ protoc, "-I/src/protos", "-I.", "--python_out=.", "protos/command.proto" ]
+    
+    if subprocess.call(protoc_command) != 0:
+        sys.exit(-1)
+
+    shutil.move("protos/command_pb2.py", "src/protos/command_pb2.py")
+
+def generate_debug():
+    protoc_command = [ protoc, "-I/src/protos", "-I.", "--python_out=.", "protos/debug.proto" ]
+    
+    if subprocess.call(protoc_command) != 0:
+        sys.exit(-1)
+
+    shutil.move("protos/debug_pb2.py", "src/protos/debug_pb2.py")
+
+def generate_control():
+    protoc_command = [ protoc, "-I/src/protos", "-I.", "--python_out=.", "protos/control.proto" ]
+    
+    if subprocess.call(protoc_command) != 0:
+        sys.exit(-1)
+
+    shutil.move("protos/control_pb2.py", "src/protos/control_pb2.py")
+
+if __name__ == '__main__':
+    generate_state()
+    generate_control()
+    generate_command()
+    generate_debug()
+
+    setup(
+        name='vsscorepy',
+        version='0.1.1',
+        packages=find_packages(),
+        license='GPL3',
+        url='https://vss-sdk.github.io/',
+        author_email='johnathanfercher22@gmail.com',
+        keywords=["IEEE", "VSS", "Robot Soccer", "VSS-SDK"],
+    )
